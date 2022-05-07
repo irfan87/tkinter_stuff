db4be4d08799de66446153608be97044a67b3176
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2022-05-07 16:12:36 +0800

    tkinter general stuff first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TKINTER STUFF
+
+Tkinter is one of Python GUI that allows us to develop Python application.
---
+++ tkinter_stuff/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+# creating a label widget
+myLabel = Label(root, text="Hello World")
+# shoving label widget into the screen
+myLabel.pack()
+
+root.mainloop()
